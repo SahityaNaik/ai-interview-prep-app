3231a665ac0bcf0c2b7d14f2e863064fdbf9d5a5
SahityaNaik <sahityanaik03@gmail.com>
2025-10-23 17:46:40 +0530

    Added base backend url to api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // change to Render URL after deployment
+  baseURL: "https://ai-interview-prep-app-nos1.onrender.com", 
 });
 
 api.interceptors.request.use((config) => {
